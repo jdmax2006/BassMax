3a0ab677deada3c02627586c1614084a5c01d8c8
jdmax <jdmax2006@gmail.com>
2025-05-21 11:46:23 -0400

    70%

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function  CrearHTMLCancion (data){
   div.addEventListener('click', function () {
    document.getElementById('titulo').innerHTML= data.titulo
    //agregamos un id al author, y repetimos , buscamos el elemento por la id y le cambiamos el innerhtml
-   
+   document.getElementById('audio').setAttribute('src', data.audio.url)
   })
   div.setAttribute('class', 'card')
   div.innerHTML = `<img src="${data.image.url}" alt="" id="song_1_cover">
   <h3 id="song_1_name">${data.title}</h3>
   <p id="song_1_artist">${data.author}</p>
-  <button class="play"><a href="Casete.html">Reproducir</a></button>`
+  <button class="play"><a href="">Reproducir</a></button>`
 
   return div
 }
