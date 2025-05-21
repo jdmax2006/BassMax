dfc2c333892bb6667a9da75fac70ec921f87646e
jdmax <jdmax2006@gmail.com>
2025-05-21 11:17:02 -0400

    60%

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const contenedorCanciones = document.getElementById('playlist')
 
 function  CrearHTMLCancion (data){
+  const div = document.createElement('li')
+  div.addEventListener('click', function () {
+   document.getElementById('titulo').innerHTML= data.titulo
+   //agregamos un id al author, y repetimos , buscamos el elemento por la id y le cambiamos el innerhtml
+   
+  })
+  div.setAttribute('class', 'card')
+  div.innerHTML = `<img src="${data.image.url}" alt="" id="song_1_cover">
+  <h3 id="song_1_name">${data.title}</h3>
+  <p id="song_1_artist">${data.author}</p>
+  <button class="play"><a href="Casete.html">Reproducir</a></button>`
 
+  return div
 }
 
 // Make a request for a user with a given ID
@@ -10,7 +22,7 @@ axios.get('https://api.institutoalfa.org/api/songs')
     // handle success
     console.log(response.data.songs);
 
-    response.data.songs.map(function () {
+    response.data.songs.map(function (data) {
         contenedorCanciones.appendChild(CrearHTMLCancion(data))
     })
   })
